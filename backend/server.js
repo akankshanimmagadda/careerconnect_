fbae863cb88949b4956e2c67914de3630876ba02
akanksha <akankshanimmagadda@gmai.com>
2026-03-25 22:20:02 +0530

    logs added for aws

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+console.log("ENV CHECK:", {
+  bucket: process.env.AWS_S3_BUCKET_NAME,
+  region: process.env.AWS_REGION,
+  key: process.env.AWS_ACCESS_KEY_ID ? "present" : "missing",
+});
+
 import app from "./app.js";
 import http from "http";
 import { setupSocket } from "./socket.js";
